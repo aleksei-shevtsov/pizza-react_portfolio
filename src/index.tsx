7b15faa929568d37e513a4a893c1a09850197e4b
Aleksey Shevtsov <shevtsov.alexey.dev@gmail.com>
2023-03-10 13:42:33 +0300

    change styles + Hash provider for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import App from './App';
+import App from "./App";
 
-import { store } from './redux/store';
+import { store } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>
+  // </BrowserRouter>,
 );
